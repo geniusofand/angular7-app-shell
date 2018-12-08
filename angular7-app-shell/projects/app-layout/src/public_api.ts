c37a860934da6e1c6a74c4f50552cf7afe12cad3
thompsa6 <thompsa6@gmail.com>
2018-12-08 15:58:38 -0700

    clean up of AppLayoutModule

--- angular7-app-shell/projects/app-layout/src/public_api.ts
+++ angular7-app-shell/projects/app-layout/src/public_api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of app-layout
  */
 
-export * from './lib/app-layout.service';
 export * from './lib/components/index';
 export * from './lib/constants/index';
 export * from './lib/app-layout.module';
