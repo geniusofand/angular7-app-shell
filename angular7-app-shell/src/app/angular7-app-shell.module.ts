2ec724a4fba75f01c7c127744eb1d9593aa7ffce
thompsa6 <thompsa6@gmail.com>
2018-12-08 22:27:40 -0700

    stable commit for the example consuming app

--- angular7-app-shell/src/app/angular7-app-shell.module.ts
+++ angular7-app-shell/src/app/angular7-app-shell.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+
+// import { AppRouter } from 'app-router';
+import { TraceLogModule } from 'trace-log';
 
 import { AppLayoutModule } from './app-layout/app-layout.module';
 
 import { Angular7AppShellRoutingModule } from './angular7-app-shell-routing.module';
 import { Angular7AppShellComponent } from './angular7-app-shell.component';
 
+
 @NgModule({
   declarations: [
     Angular7AppShellComponent
   ],
   imports: [
+    StoreModule.forRoot({router: routerReducer}),
     Angular7AppShellRoutingModule,
+    StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
     AppLayoutModule,
     BrowserModule,
+    TraceLogModule,
   ],
   providers: [],
   bootstrap: [Angular7AppShellComponent]
